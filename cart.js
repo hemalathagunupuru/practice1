290cdf469bc69f754fe99776f799210a1cff0b82
hemalathagunupuru <114648728+hemalathagunupuru@users.noreply.github.com>
2022-10-31 17:08:26 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,11 @@
+let cartIcon = document.querySelector("cart-icon");
+let cart = document.querySelector(".cart");
+lat closeCart = document.querySelector("#close-cart");
+
+cartIcon.onclick = () => {
+cartIcon.onclickList.add("active");
+};
+
+cartIcon.onclick = () => {
+cartIcon.onclickList.remove("active");
+};
